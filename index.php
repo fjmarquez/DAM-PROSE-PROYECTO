8590187d506bf341c4ec2c0b024c20468222753d
Francisco José Márquez Pulido <43822565+fjmarquez@users.noreply.github.com>
2020-11-07 19:08:58 +0100

    Pruebas en pagina Productos

--- index.php
+++ index.php
@@ -1,28 +1,25 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <meta charset="UTF-8">
-        <title>Login</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="styles/style.css">
-        <script
-            src="https://code.jquery.com/jquery-3.5.1.min.js"
-            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-            crossorigin="anonymous">
-        </script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-                integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+            <meta charset="UTF-8">
+            <title>Login</title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+            <link rel="stylesheet" href="styles/style.css">
+            <script
+                src="https://code.jquery.com/jquery-3.5.1.min.js"
+                integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
                 crossorigin="anonymous">
-        </script>
-
+            </script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+                    integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+                    crossorigin="anonymous">
+            </script>
     </head>
-
-    <body>
+    <body class="body-login">
         <div class="container-fluid">
             <div class="row">
                 <div class="jumbotron margintop col-md-4 col-md-offset-4">
                     
-                  
                     <form action="action/actionLogin.php" method="post">
                         <div class=" center">
                             <img class="img-responsive center-block logo" src="img/logo.png" alt="logo">
@@ -42,7 +39,7 @@
                         </div>
 
                         <div class="text-center flex ">
-                            <button class="btn" type="submit">Sign in</button>
+                            <button class="btn btn-login" type="submit">Sign in</button>
                         </div>
 
                         <?php
@@ -50,8 +47,6 @@
                         if(isset($_GET['error'])){
                             echo '<p class="datosIncorrectos">Datos incorrectos</p>';
                         }
-                        
-                            
                         
                         ?>
                         
---
+++ pages/productos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+    <?php
+        require_once('partials/head.php');
+    ?>
+    <body>
+        
+                <?php
+                    require('partials/navbar.php');
+                ?>
+        
+    </body>
+</html>
