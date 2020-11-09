37cc83249875364769d4ca838debfc1c3f87e53a
ManuelCaballero <emanenxd@gmail.com>
2020-11-09 19:07:40 +0100

    Producto Admin

--- form/productoAdmin.php
+++ form/productoAdmin.php
@@ -10,48 +10,63 @@ $categorias = $cDAO->obtenerTodasLasCategorias();
 //var_dump($producto);
 ?>
 
-<form class="col-md-8">
+<div class="col-md-5">
+    <img class="card-img" src="<?= $producto->getImage() ?>"
+         title="<?= $producto->getName() . " - " . $producto->getCategory()->getCategory() ?>">
+</div>
+
+<form class="col-md-7">
+    <input id="IDProducto" name="IDProducto" hidden value="<?= $pID ?>">
     <div class="form-group row">
         <label class="col-md-4" for="nombreProducto">Nombre: </label>
-        <input id="nombreProducto" name="nombreProducto" class="form-control col-md-8" type="text" maxlength="100">
+        <input id="nombreProducto" name="nombreProducto" class="form-control col-md-8" type="text" maxlength="100" value="<?= $producto->getName() ?>">
     </div>
     <div class="form-group row">
         <label class="col-md-4" for="stockProducto">Stock: </label>
-        <input id="stockProducto" name="stockProducto" class="form-control col-md-8" type="number" min="0" max="99999999999">
+        <input id="stockProducto" name="stockProducto" class="form-control col-md-8" type="number" min="0" max="99999999999" value="<?= $producto->getStock() ?>">
     </div>
     <div class="form-group row">
         <label class="col-md-4" for="precioProducto">Precio: </label>
-        <input id="precioProducto" name="precioProducto" class="form-control col-md-7" type="number" step=".01" min="1">
+        <input id="precioProducto" name="precioProducto" class="form-control col-md-7" type="number" step=".01" min="1" value="<?= $producto->getPrice() ?>">
         <div class="input-group-prepend col-md-1">
             <span class="input-group-text" id="basic-addon1">€</span>
         </div>
     </div>
     <div class="form-group row">
         <label class="col-md-4" for="descuentoProducto">Descuento: </label>
-        <input id="descuentoProducto" name="descuentoProducto" class="form-control col-md-8" type="number" step=".1" min="0" max="1">
+        <input id="descuentoProducto" name="descuentoProducto" class="form-control col-md-8" type="number" step=".1" min="0" max="1" value="<?= $producto->getDiscount() ?>">
     </div>
     <div class="form-group row">
         <label class="col-md-4" for="descripcionProducto">Descripción: </label>
-        <input id="descripcionProducto" name="descripcionProducto" class="form-control col-md-8" type="text" maxlength="900">
+        <textarea id="descripcionProducto" name="descripcionProducto" class="form-control col-md-8" maxlength="900"><?= $producto->getLongDescription() ?> </textarea>
     </div>
     <div class="form-group row">
         <label class="col-md-4" for="primeProducto">Prime: </label>
-        <input id="primeProducto" name="Prime" type="checkbox">
+        <input id="primeProducto" name="Prime" type="checkbox"
+            <?php
+                if($producto->getPrime() == 1)
+                {
+                    echo "checked";
+                }
+            ?>>
     </div>
     <div class="form-group row">
         <select id="categoriaProducto" name="categoriaProducto" class="form-control">
-            <option disabled selected>Categoría</option>
+            <option disabled>Categoría</option>
             <?php
                 foreach ($categorias as $categoria)
                 {
                     ?>
-                    <option><?= $categoria->getCategory() ?></option>
+                        <option <?php if($categoria->getID() == $producto->getCategory()->getID()){echo "selected";}?> ><?= $categoria->getCategory() ?></option>
                     <?php
                 }
             ?>
         </select>
     </div>
     <div class="form-group row">
-       <input class="form-control" id="imagenProducto" name="imagenProducto" type="file">
+        <button class="btn btn-primary" type="submit">Guardar</button>
+        <button class="btn btn-dark" type="reset">Cancelar</button>
     </div>
 </form>
+
+
